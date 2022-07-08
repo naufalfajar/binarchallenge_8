0923e47e890ed6484318ac3d19005bb972c40c19
naufalfajar <naufalphew@gmail.com>
2022-07-08 19:50:29 +0700

    add lint

--- app/src/main/java/id/nphew/binar/challenge8/ui/theme/HomeUI.kt
+++ app/src/main/java/id/nphew/binar/challenge8/ui/theme/HomeUI.kt
@@ -1,15 +1,47 @@
 package id.nphew.binar.challenge8.ui.theme
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Preview(showBackground = true)
 @Composable
 fun HomeScreen(){
-
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Spacer(modifier = Modifier.height(20.dp))
+        Welcome()
+    }
 }
 
 @Composable
 fun Welcome(){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()) {
+        Text(text = "Welcome, ",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold)
 
+        Text(text = "Logout",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold)
+    }
 }
